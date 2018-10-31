1e40b2b3603316a2698eadf7b66045b303583fa1
abhinav0397 <abhi7.nair@gmail.com>
2018-10-31 16:45:45 +0000

    Error spread

--- 5762.R
+++ 5762.R
@@ -89,47 +89,69 @@ for(i in cols_to_change){
 }
 cols_to_change
 
-# Fitting a model for mother's weight and race against baby weight
+# Fitting models for  certain variables against baby weight
 
-# Linear model between race, mother weight against baby weight
+# Linear model between race and mother weight against baby weight
 
 race_wt.1 <- lm(wt ~ race*wt.1, data = data)
 summary(race_wt.1)
+anova(race_wt.1)
 
 # Linear model between mother's weight and smoking against baby weight 
 smoke_wt.1 <- lm(wt ~ smoke*wt.1, data = data)
 summary(smoke_wt.1)
+anova(smoke_wt.1)
 
 # Linear model between mother's weight and parity against baby weight 
 parity_wt.1 <- lm(wt ~ parity*wt.1, data = data)
 summary(parity_wt.1)
+anova(parity_wt.1)
 
 # Linear model between mother's weight and time against baby weight
 time_wt.1 <- lm(wt ~ time*wt.1, data = data)
 summary(time_wt.1)
+anova(time_wt.1)
 
 # Linear model between mother's weight and income against baby weight
 inc_wt.1 <- lm(wt ~ inc*wt.1, data = data)
 summary(inc_wt.1)
+anova(inc_wt.1)
+
 
 # Model diagnostics for each model built
-# Error shape of model between race, mother weight against baby weight
+# Error shape and distribution of model between race and mother weight against baby weight
 qqnorm(resid(race_wt.1))
 shapiro.test(resid(race_wt.1))
 hist(resid(race_wt.1))
 
-# Error shape of model between mother's weight and smoking against baby weight
+# Error shape and distribution of model between mother's weight and smoking against baby weight
 qqnorm(resid(smoke_wt.1))
 shapiro.test(resid(smoke_wt.1))
 hist(resid(smoke_wt.1))
 
-# Error shape of model between mother's weight and parity against baby weight
+# Error shape and distribution of model between mother's weight and parity against baby weight
 qqnorm(resid(parity_wt.1))
 shapiro.test(resid(parity_wt.1))
 hist(resid(parity_wt.1))
 
-# Error shape of  model between mother's weight and income against baby weight
+# Error shape and distribution of  model between mother's weight and income against baby weight
 qqnorm(resid(inc_wt.1))
 shapiro.test(resid(inc_wt.1))
 hist(resid(inc_wt.1))
+
+# Error spread of model between race, mother weight against baby weight
+race_resid <- resid(race_wt.1)
+plot(fitted(race_wt.1), race_resid, ylab = 'residuals', xlab = 'Fitted values')
+
+# Error spread of model between mother's weight and smoking against baby weight
+smoke_resid <- resid(smoke_wt.1)
+plot(fitted(smoke_wt.1), smoke_resid, ylab = 'residuals', xlab = 'Fitted values')
+
+# Error spread of model between mother's weight and parity against baby weight
+parity_resid <- resid(parity_wt.1)
+plot(fitted(parity_wt.1), parity_resid, ylab = 'residuals', xlab = 'Fitted values')
+
+# Error spread of model between mother's weight and income against baby weight
+inc_resid <- resid(inc_wt.1)
+plot(fitted(inc_wt.1), inc_resid, ylab = 'residuals', xlab = 'Fitted values')
 
