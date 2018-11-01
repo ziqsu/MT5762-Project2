6cca25d9941b2b249d1ef98cb3f48d4094b12123
PearL <PearL@8afbf716.st-andrews.ac.uk>
2018-11-01 16:16:56 +0000

    cross-validation

--- 5-folder cross validation.R
+++ 5-folder cross validation.R
@@ -9,6 +9,8 @@ library(boot)
 MSE_5_folder_cv <- cv.lm(babies.data,finalModel, m = 5)$delta[1]
 MSE_5_folder_cv
 
+
+
 # Import the library MASS for dataset and caret for crossvalidation
 library(MASS, quietly = TRUE)
 library(caret)
@@ -46,3 +48,62 @@ parameterGrid <- expand.grid(mtry=c(1,2,3,4,5))
 
 modelRandom <- train (wt~gestation, data = trainDF, method = "rf", trControl = ControlParameters, tuneGrid = parameterGrid)
 
+
+
+
+
+
+
+
+
+###########################
+library(caret)
+data(babies.data)
+set.seed(62)
+
+model <- train (wt~dwt, babies.data, method = "lm", trControl=trainControl(method="cv",number = 5, verboseIter = TRUE))
+
+
+
+
+
+
+
+
+#########################
+require(caret)
+classes <- babies.data [,"wt"]
+predictors <- babies.data [, -match(c("gestation","ht","ded","dwt","inc","time","number","inc:parity","wt.1:ed","ed:drace","time:ed","ded:smoke"),colnames(babies.data))]
+
+train_set <- createDataPartition(classes, p= 0.8, list = FALSE)
+str(train_set)
+
+train_predictors <- predictors [train_set,]
+train_classes <- classes [train_set]
+test_predictors <- predictors [-train_set,]
+test_classes <- classes [-train_set]
+
+set.seed(6262)
+cv_splits <- createFolds(classes, k= 5, returnTrain = TRUE)
+str(cv_splits)
+
+
+######################
+require(glmnet)
+
+set.seed(5757)
+
+cs_data_train <- cs_data[train_set, ]
+cs_data_test <- cs_data[-train_set, ]
+
+glmnet_grid <- expand.grid(alpha = c(0,  .1,  .2, .4, .6, .8, 1),
+                           lambda = seq(.01, .2, length = 20))
+glmnet_ctrl <- trainControl(method = "cv", number = 5)
+glmnet_fit <- train(wt ~ ., data = cs_data_train,
+                    method = "glmnet",
+                    preProcess = c("gestation","ht","ded","dwt","inc","time","number","inc:parity","wt.1:ed","ed:drace","time:ed","ded:smoke"),
+                    tuneGrid = glmnet_grid,
+                    trControl = glmnet_ctrl)
+
+trellis.par.set(caretTheme())
+plot(glmnet_fit, scales = list(x=list(log =2)))
