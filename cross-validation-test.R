46a92268437ef5a9b549d22b41cf256b7bce2c27
PearL <PearL@8afbfa9a.st-andrews.ac.uk>
2018-11-01 21:12:19 +0000

    add another way

--- cross-validation-test.R
+++ cross-validation-test.R
@@ -15,3 +15,27 @@ for(i in 1:10){
 }
 mean(re)
 
+
+
+###########
+#Another way
+library(caTools)
+set.seed(110)
+split <- sample.split(clean.data.naomit$wt, SplitRatio = 0.8)
+training_set <- subset(clean.data.naomit, split = TRUE)
+test_set <- subset(clean.data.naomit, split = FALSE)
+
+#Applying 5-fold cross validation
+#install.packages("caret")
+library(caret)
+folds <-createFolds(training_set$wt, k = 5)
+cv <- lapply(folds, function(x){
+  training_fold <- training_set[-x,]
+  test_fold <- training_set[x,]
+  dataModel <- lm(wt ~., data = training_fold)
+  dataModel <- step(dataModel)
+  y_pred <- predict (dataModel, newdata = test_fold[-3])
+  cm <- table(test_fold[,3],y_pred)
+  accuracy <- (cm[1,1] + cm [2,2]) / (cm[1,1] + cm [2,2] + cm[1,2] + cm [2,1])
+  return(accuracy)
+})
